6795b8f248f28252ca38b51a3d7ec2d31338f692
José Airton Neto <jose.neto.airton@gmail.com>
2025-07-03 22:20:08 -0300

    Páginas e rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary' />
-      <Button variant='secondary' />
-      <Button variant='danger' />
-      <Button variant='success' />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
